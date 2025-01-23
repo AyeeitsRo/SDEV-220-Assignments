57314521a6fd7a3ccdc65634d4cc0efe00945c79
AyeeitsRo <164427862+AyeeitsRo@users.noreply.github.com>
2025-01-22 18:09:53 -0600

    edits

--- Module-02/M02-Test-Code.py
+++ Module-02/M02-Test-Code.py
@@ -1,15 +1,28 @@
-lastName: str = input("'ZZZ' to quit\nEnter students' last name: ")
-while lastName != 'ZZZ':
-    firstName: str = input("Enter students' first name: ")
-    gpa: float = float(input("Enter students' GPA: "))
-    if gpa >= 3.5:
+DEANS_LIST: float = 3.5
+HONOR_ROLL: float = 3.25
+EXIT: str = 'ZZZ' 
+
+lastName: str = ''
+firstName: str = ''
+
+lastName = input("'ZZZ' to quit\nEnter students' last name: ")
+while lastName != EXIT:
+    firstName = input("Enter students' first name: ")
+    while True:
+        try:
+            gpa = input("Enter students' GPA: ")
+            gpa = float(gpa)
+            break
+        except ValueError:
+            print('Please enter float point numbers.') 
+    if gpa >= DEANS_LIST:
         print(f"{firstName} {lastName} has made the Dean's List!")
-        lastName: str = input("'ZZZ' to quit\nEnter students' last name: ")
-    elif gpa >= 3.25 and gpa <= 3.49:
+        lastName = input("'ZZZ' to quit\nEnter students' last name: ")
+    elif gpa >= HONOR_ROLL and gpa < DEANS_LIST:
         print(f"{firstName} {lastName} has made Honor Roll!")
-        lastName: str = input("'ZZZ' to quit\nEnter students' last name: ")
-    elif gpa < 3.25:
+        lastName = input("'ZZZ' to quit\nEnter students' last name: ")
+    elif gpa < HONOR_ROLL:
         print(f"{firstName} {lastName} has not made Dean's List or Honor Roll.")
-        lastName: str = input("'ZZZ' to quit\nEnter students' last name: ")
+        lastName = input("'ZZZ' to quit\nEnter students' last name: ")
     else:
         break
